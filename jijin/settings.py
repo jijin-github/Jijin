ca36f7a1b991c7d1c2f627de1ba5f6e2cafcfa24
jijins <jsebastian@integretz.com>
2015-08-06 13:41:37 +0530

    new change

--- jijin/settings.py
+++ jijin/settings.py
@@ -121,3 +121,7 @@ STATICFILES_DIRS = (
 )
 
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
